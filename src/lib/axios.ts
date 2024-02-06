95caf1d1ebfe7dfde3fa45cf6ce605841aaa047b
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-02-06 08:01:25 -0300

    feat: :sparkles: implementa rapidapi e busca dados do ibovespa

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,14 @@ const awesomeApi = axios.create({
   baseURL: 'https://economia.awesomeapi.com.br/json',
 })
 
-export { awesomeApi }
+const apiKey = process.env.RAPIDAPI_KEY
+
+const yahooApi = axios.create({
+  baseURL: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2',
+  headers: {
+    'X-RapidAPI-Key': apiKey,
+    'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+  },
+})
+
+export { awesomeApi, yahooApi }
